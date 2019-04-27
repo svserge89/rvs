4c682af5d3e8418363deb1ca8fceec88fe120d1b
Serge Vorobyov <svserge89@gmail.com>
2019-04-28 01:45:37 +0300

    First implementation for RestaurantService

--- src/main/java/sergesv/rvs/service/RestaurantService.java
+++ src/main/java/sergesv/rvs/service/RestaurantService.java
@@ -3,12 +3,17 @@ package sergesv.rvs.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import sergesv.rvs.model.Restaurant;
 import sergesv.rvs.repository.RestaurantRepository;
 import sergesv.rvs.repository.VoteEntryRepository;
 import sergesv.rvs.web.to.RestaurantTo;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static sergesv.rvs.util.ToUtil.toModel;
+import static sergesv.rvs.util.ToUtil.toTo;
 
 @Service
 @Transactional(readOnly = true)
@@ -18,98 +23,145 @@ public class RestaurantService {
     private final VoteEntryRepository voteEntryRepository;
 
     public List<RestaurantTo> getAll() {
-        return null;
+        return restaurantRepository.findAll().stream()
+                .map(restaurant -> toTo(restaurant, false))
+                .collect(Collectors.toList());
     }
 
     public List<RestaurantTo> getAllWithRating() {
-        return null;
+        return restaurantRepository.findAll().stream()
+                .map(restaurant -> toTo(restaurant, false,
+                        voteEntryRepository.countByRestaurant(restaurant)))
+                .collect(Collectors.toList());
     }
 
     public List<RestaurantTo> getAllWithRating(LocalDate date) {
-        return null;
+        return restaurantRepository.findAll().stream()
+                .map(restaurant -> toTo(restaurant, false,
+                        voteEntryRepository.countByRestaurantAndDateEquals(restaurant, date)))
+                .collect(Collectors.toList());
     }
 
     public List<RestaurantTo> getAllWithRating(LocalDate dateStart, LocalDate dateEnd) {
-        return null;
+        return restaurantRepository.findAll().stream()
+                .map(restaurant -> toTo(restaurant, false,
+                        voteEntryRepository.countByRestaurantAndDateBetween(restaurant, dateStart,
+                                dateEnd)))
+                .collect(Collectors.toList());
     }
 
     public List<RestaurantTo> getAllWithMenu(LocalDate menuDate) {
-        return null;
+        return restaurantRepository.findAllWithMenu(menuDate).stream()
+                .map(restaurant -> toTo(restaurant, true))
+                .collect(Collectors.toList());
     }
 
     public List<RestaurantTo> getAllWithMenuAndRating(LocalDate menuDate) {
-        return null;
+        return restaurantRepository.findAllWithMenu(menuDate).stream()
+                .map(restaurant -> toTo(restaurant, true,
+                        voteEntryRepository.countByRestaurant(restaurant)))
+                .collect(Collectors.toList());
     }
 
     public List<RestaurantTo> getAllWithMenuAndRating(LocalDate menuDate, LocalDate ratingDate) {
-        return null;
+        return restaurantRepository.findAllWithMenu(menuDate).stream()
+                .map(restaurant -> toTo(restaurant, true,
+                        voteEntryRepository.countByRestaurantAndDateEquals(restaurant, ratingDate)))
+                .collect(Collectors.toList());
     }
 
     public List<RestaurantTo> getAllWithMenuAndRating(LocalDate menuDate, LocalDate ratingDateStart,
                                                       LocalDate ratingDateEnd) {
-        return null;
+        return restaurantRepository.findAllWithMenu(menuDate).stream()
+                .map(restaurant -> toTo(restaurant, true,
+                        voteEntryRepository.countByRestaurantAndDateBetween(restaurant,
+                                ratingDateStart, ratingDateEnd)))
+                .collect(Collectors.toList());
     }
 
     public RestaurantTo getOne(long id) {
-        return null;
+        return toTo(restaurantRepository.getOne(id), false);
     }
 
     public RestaurantTo getOneWithRating(long id) {
-        return null;
+        Restaurant restaurant = restaurantRepository.getOne(id);
+
+        return toTo(restaurant, false, voteEntryRepository.countByRestaurant(restaurant));
     }
 
     public RestaurantTo getOneWithRating(long id, LocalDate date) {
-        return null;
+        Restaurant restaurant = restaurantRepository.getOne(id);
+
+        return toTo(restaurant, false,
+                voteEntryRepository.countByRestaurantAndDateEquals(restaurant, date));
     }
 
     public RestaurantTo getOneWithRating(long id, LocalDate startDate, LocalDate endDate) {
-        return null;
+        Restaurant restaurant = restaurantRepository.getOne(id);
+
+        return toTo(restaurant, false, voteEntryRepository.countByRestaurantAndDateBetween(
+                restaurant, startDate, endDate));
     }
 
     public RestaurantTo getOneWithMenu(long id, LocalDate menuDate) {
-        return null;
+        return toTo(restaurantRepository.getOneWithMenu(id, menuDate), true);
     }
 
     public RestaurantTo getOneWithMenuAndRating(long id, LocalDate menuDate) {
-        return null;
+        Restaurant restaurant = restaurantRepository.getOneWithMenu(id, menuDate);
+
+        return toTo(restaurant, true, voteEntryRepository.countByRestaurant(restaurant));
     }
 
     public RestaurantTo getOneWithMenuAndRating(long id, LocalDate menuDate, LocalDate ratingDate) {
-        return null;
+        Restaurant restaurant = restaurantRepository.getOneWithMenu(id, menuDate);
+
+        return toTo(restaurant, true,
+                voteEntryRepository.countByRestaurantAndDateEquals(restaurant, ratingDate));
     }
 
     public RestaurantTo getOneWithMenuAndRating(long id, LocalDate menuDate,
                                                 LocalDate ratingDateStart,
                                                 LocalDate ratingDateEnd) {
-        return null;
+        Restaurant restaurant = restaurantRepository.getOneWithMenu(id, menuDate);
+
+        return toTo(restaurant, true, voteEntryRepository.countByRestaurantAndDateBetween(
+                restaurant, ratingDateStart, ratingDateEnd));
     }
 
     public long getRating(long restaurantId) {
-        return 0;
+        return voteEntryRepository.countByRestaurantId(restaurantId);
     }
 
     public long getRating(long restaurantId, LocalDate date) {
-        return 0;
+        return voteEntryRepository.countByRestaurantIdAndDateEquals(restaurantId, date);
     }
 
     public long getRating(long restaurantId, LocalDate dateStart, LocalDate dateEnd) {
-        return 0;
+        return voteEntryRepository.countByRestaurantIdAndDateBetween(restaurantId, dateStart,
+                dateEnd);
     }
 
     @Transactional
     public RestaurantTo create(RestaurantTo restaurantTo) {
-        return null;
+        return toTo(restaurantRepository.save(toModel(restaurantTo)), false);
     }
 
     @Transactional
     public void update(long id, RestaurantTo restaurantTo) {
+        Restaurant restaurant = toModel(restaurantTo);
+
+        restaurant.setId(id);
+        restaurantRepository.save(restaurant);
     }
 
     @Transactional
     public void delete(long id) {
+        restaurantRepository.deleteById(id);
     }
 
     @Transactional
     public void deleteAll() {
+        restaurantRepository.deleteAll();
     }
 }
