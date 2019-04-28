e51cb9568a95a82d6ca6c9cf75e98ab5d01e6414
Serge Vorobyov <svserge89@gmail.com>
2019-04-28 10:42:59 +0300

    Add optimized queries for RestaurantService

--- src/main/java/sergesv/rvs/model/VoteEntry.java
+++ src/main/java/sergesv/rvs/model/VoteEntry.java
@@ -8,6 +8,25 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Entity
+@NamedQueries({
+        @NamedQuery(name = VoteEntry.GET_RATING_PAIRS,
+            query = "SELECT new sergesv.rvs.model.RatingPair(voteEntry.restaurant, " +
+                                                            "COUNT (voteEntry))" +
+                    "FROM VoteEntry voteEntry " +
+                    "GROUP BY voteEntry.restaurant"),
+        @NamedQuery(name = VoteEntry.GET_RATING_PAIRS_BY_DATE,
+            query = "SELECT new sergesv.rvs.model.RatingPair(voteEntry.restaurant, " +
+                                                            "COUNT (voteEntry))" +
+                    "FROM VoteEntry voteEntry " +
+                    "WHERE voteEntry.date = :date " +
+                    "GROUP BY voteEntry.restaurant"),
+        @NamedQuery(name = VoteEntry.GET_RATING_PAIRS_BY_DATE_BETWEEN,
+            query = "SELECT new sergesv.rvs.model.RatingPair(voteEntry.restaurant, " +
+                                                            "COUNT (voteEntry))" +
+                    "FROM VoteEntry voteEntry " +
+                    "WHERE voteEntry.date BETWEEN :dateStart AND :dateEnd " +
+                    "GROUP BY voteEntry.restaurant")
+})
 @Table(name = "vote_entry")
 @Getter
 @Setter
@@ -16,6 +35,11 @@ import java.time.LocalTime;
 @ToString
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class VoteEntry {
+    public static final String GET_RATING_PAIRS = "VoteEntry.getRatingPairs";
+    public static final String GET_RATING_PAIRS_BY_DATE = "VoteEntry.getRatingPairsByDate";
+    public static final String GET_RATING_PAIRS_BY_DATE_BETWEEN =
+            "VoteEntry.getRatingPairsByDateBetween";
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
--- src/main/java/sergesv/rvs/repository/VoteEntryRepository.java
+++ src/main/java/sergesv/rvs/repository/VoteEntryRepository.java
@@ -1,20 +1,37 @@
 package sergesv.rvs.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import sergesv.rvs.model.Restaurant;
+import sergesv.rvs.model.RatingPair;
 import sergesv.rvs.model.VoteEntry;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 @Repository
 public interface VoteEntryRepository extends JpaRepository<VoteEntry, Long> {
     long countByRestaurant(Restaurant restaurant);
+
     long countByRestaurantAndDateEquals(Restaurant restaurant, LocalDate date);
+
     long countByRestaurantAndDateBetween(Restaurant restaurant, LocalDate dateStart,
                                          LocalDate dateEnd);
+
     long countByRestaurantId(Long restaurantId);
+
     long countByRestaurantIdAndDateEquals(Long restaurantId, LocalDate date);
+
     long countByRestaurantIdAndDateBetween(Long restaurantId, LocalDate dateStart,
                                            LocalDate dateEnd);
+
+    @Query(name = VoteEntry.GET_RATING_PAIRS)
+    Set<RatingPair> getRatingPairs();
+
+    @Query(name = VoteEntry.GET_RATING_PAIRS_BY_DATE)
+    Set<RatingPair> getRatingPairsByDate(LocalDate date);
+
+    @Query(name = VoteEntry.GET_RATING_PAIRS_BY_DATE_BETWEEN)
+    Set<RatingPair> getRatingPairsByDateBetween(LocalDate dateStart, LocalDate dateEnd);
 }
