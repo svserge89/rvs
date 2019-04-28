409fe88108057f5d2c78eadd2b82bb096b67fda5
Serge Vorobyov <svserge89@gmail.com>
2019-04-28 12:07:11 +0300

    First implementation for MenuEntryService

--- src/main/java/sergesv/rvs/service/MenuEntryService.java
+++ src/main/java/sergesv/rvs/service/MenuEntryService.java
@@ -3,12 +3,18 @@ package sergesv.rvs.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import sergesv.rvs.model.Restaurant;
 import sergesv.rvs.repository.MenuEntryRepository;
 import sergesv.rvs.repository.RestaurantRepository;
+import sergesv.rvs.util.ToUtil;
 import sergesv.rvs.web.to.MenuEntryTo;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static sergesv.rvs.util.ToUtil.toModel;
+import static sergesv.rvs.util.ToUtil.toTo;
 
 @Service
 @Transactional(readOnly = true)
@@ -18,31 +24,42 @@ public class MenuEntryService {
     private final RestaurantRepository restaurantRepository;
 
     public List<MenuEntryTo> getAllByRestaurant(long restaurantId, LocalDate date) {
-        return null;
+        return menuEntryRepository.findAllByRestaurantIdAndDate(restaurantId, date).stream()
+                .map(ToUtil::toTo)
+                .collect(Collectors.toList());
     }
 
     public MenuEntryTo getOneByRestaurant(long id, long restaurantId) {
-        return null;
+        return toTo(menuEntryRepository.getByIdAndRestaurantId(id, restaurantId));
     }
 
     @Transactional
     public MenuEntryTo create(MenuEntryTo menuEntryTo, long restaurantId) {
-        return null;
+        Restaurant restaurant = restaurantRepository.getOne(restaurantId);
+
+        return toTo(menuEntryRepository.save(toModel(menuEntryTo, restaurant)));
     }
 
     @Transactional
     public void update(long id, MenuEntryTo menuEntryTo, long restaurantId) {
+        Restaurant restaurant = restaurantRepository.getOne(restaurantId);
+
+        menuEntryRepository.save(toModel(id, menuEntryTo, restaurant));
     }
 
     @Transactional
     public void deleteByRestaurant(long id, long restaurantId) {
+        menuEntryRepository.deleteByIdAndRestaurantId(id, restaurantId);
     }
 
     @Transactional
     public void deleteAllByRestaurant(long restaurantId, LocalDate date) {
+        menuEntryRepository.deleteAllByRestaurantIdAndDate(restaurantId, date);
     }
 
     @Transactional
     public void deleteAllByRestaurant(long restaurantId, LocalDate dateStart, LocalDate dateEnd) {
+        menuEntryRepository.deleteAllByRestaurantIdAndDateBetween(restaurantId, dateStart,
+                dateEnd);
     }
 }
