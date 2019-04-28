6e3a7796c4b74d80eae64a443286b81380e4ed4b
Serge Vorobyov <svserge89@gmail.com>
2019-04-28 16:03:44 +0300

    Configure spring-security for basic authorization

--- src/main/java/sergesv/rvs/configuration/SecurityConfiguration.java
+++ src/main/java/sergesv/rvs/configuration/SecurityConfiguration.java
@@ -2,7 +2,9 @@ package sergesv.rvs.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -29,8 +32,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/api/public/**").permitAll()
-                .antMatchers("/api/admin/**").hasRole("ADMIN")
-                .antMatchers("/api/user/**").hasRole("USER");
+        http
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/public/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/admin/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/api/admin/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/admin/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/admin/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/user/**").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/api/user/**").hasRole("USER")
+                .antMatchers(HttpMethod.DELETE, "/api/user/**").hasRole("USER")
+                .and()
+                .csrf().disable()
+                .formLogin().disable();
     }
 }
