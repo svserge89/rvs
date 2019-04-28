28c81bc867ac61df50ee7264a7fc4e49dd4dfbf2
Serge Vorobyov <svserge89@gmail.com>
2019-04-28 12:06:22 +0300

    Add methods for MenuEntryService

--- src/main/java/sergesv/rvs/repository/MenuEntryRepository.java
+++ src/main/java/sergesv/rvs/repository/MenuEntryRepository.java
@@ -4,6 +4,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sergesv.rvs.model.MenuEntry;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface MenuEntryRepository extends JpaRepository<MenuEntry, Long> {
+    List<MenuEntry> findAllByRestaurantIdAndDate(Long restaurantId, LocalDate date);
+
+    MenuEntry getByIdAndRestaurantId(Long id, Long restaurantId);
+
+    void deleteByIdAndRestaurantId(Long id, Long restaurantId);
+
+    void deleteAllByRestaurantIdAndDate(Long restaurantId, LocalDate date);
+
+    void deleteAllByRestaurantIdAndDateBetween(Long restaurantId, LocalDate dateStart,
+                                                 LocalDate dateEnd);
 }
