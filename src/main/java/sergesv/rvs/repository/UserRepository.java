9d98e92697df069e472130f8c4153bf568a37842
Serge Vorobyov <svserge89@gmail.com>
2019-04-28 18:02:51 +0300

    Update methods for authorization by nickname and email

--- src/main/java/sergesv/rvs/repository/UserRepository.java
+++ src/main/java/sergesv/rvs/repository/UserRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sergesv.rvs.model.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User getByEmail(String email);
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByNickName(String nickname);
 
     void deleteAllByIdNot(Long id);
 }
