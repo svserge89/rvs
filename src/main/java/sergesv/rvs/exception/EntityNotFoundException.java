37361bbf245ceceee7c28c2e21d16b8ad4549011
Serge Vorobyov <svserge89@gmail.com>
2019-04-30 06:19:35 +0300

    Create EntityNotFoundException

---
+++ src/main/java/sergesv/rvs/exception/EntityNotFoundException.java
@@ -0,0 +1,23 @@
+package sergesv.rvs.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException() {
+        super();
+    }
+
+    public EntityNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+    public EntityNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
