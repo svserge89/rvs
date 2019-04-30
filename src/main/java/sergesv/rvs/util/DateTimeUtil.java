9977d993ebe4b38c0ab7fda68ea01eb952b8f3f0
Serge Vorobyov <svserge89@gmail.com>
2019-04-30 22:43:38 +0300

    Change access to package for MAX_CHANGE_TIME constant

--- src/main/java/sergesv/rvs/util/DateTimeUtil.java
+++ src/main/java/sergesv/rvs/util/DateTimeUtil.java
@@ -7,7 +7,7 @@ public final class DateTimeUtil {
     public static final LocalDate MIN_DATE = LocalDate.of(1, 1, 1);
     public static final LocalDate MAX_DATE = LocalDate.of(3000, 1, 1);
 
-    private static final LocalTime MAX_CHANGE_TIME = LocalTime.of(11, 0);
+    static final LocalTime MAX_CHANGE_TIME = LocalTime.of(11, 0);
 
     public static boolean checkTime(LocalTime localTime) {
         return localTime.getSecond() <= MAX_CHANGE_TIME.getSecond();
