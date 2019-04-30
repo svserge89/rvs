97ba2eaf10ca9b317150743dd742eb381c794e41
Serge Vorobyov <svserge89@gmail.com>
2019-04-30 09:47:16 +0300

    Add methods for check Not Found

--- src/main/java/sergesv/rvs/util/ValidationUtil.java
+++ src/main/java/sergesv/rvs/util/ValidationUtil.java
@@ -5,9 +5,28 @@ import sergesv.rvs.exception.EntityNotFoundException;
 import java.util.function.Supplier;
 
 public final class ValidationUtil {
-    public static Supplier<EntityNotFoundException> getNotFoundSupplier(long id) {
+    public static Supplier<EntityNotFoundException> restaurantNotFoundSupplier(long menuEntryId) {
         return () -> new EntityNotFoundException(
-                String.format("Not found entity with id '%d'", id));
+                String.format("Not found Restaurant entity with id = %d", menuEntryId));
+    }
+
+    public static Supplier<EntityNotFoundException> menuEntryNotFoundSupplier(long menuEntryId,
+                                                                              long restaurantId) {
+        return () -> new EntityNotFoundException(
+                String.format("Not found MenuEntry entity with id = %d and restaurantId = %d",
+                        menuEntryId, restaurantId));
+    }
+
+    public static Supplier<EntityNotFoundException> userNotFoundSupplier(long userId) {
+        return () -> new EntityNotFoundException(
+                String.format("Not found User with id = %d", userId));
+    }
+
+    public static void checkExists(boolean exists,
+                                   Supplier<? extends RuntimeException> supplier) {
+        if (!exists) {
+            throw supplier.get();
+        }
     }
 
     private ValidationUtil() {
