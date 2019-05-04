b64fe7e5120124bdab85e8ec59dac645db465f4b
Serge Vorobyov <svserge89@gmail.com>
2019-05-04 18:21:15 +0300

    Refactoring RvsPropertyResolver and add logging for all properties

--- src/main/java/sergesv/rvs/RvsPropertyResolver.java
+++ src/main/java/sergesv/rvs/RvsPropertyResolver.java
@@ -18,6 +18,17 @@ import static sergesv.rvs.util.SortUtil.*;
 @ConfigurationProperties(prefix = "rvs")
 @Setter
 public class RvsPropertyResolver {
+    private static final String PROPERTY_RESTAURANT_PAGE_SIZE = "rvs.restaurant-page-size";
+    private static final String PROPERTY_MENU_ENTRY_PAGE_SIZE = "rvs.menu-entry-page-size";
+    private static final String PROPERTY_USER_PAGE_SIZE = "rvs.user-page-size";
+    private static final String PROPERTY_VOTE_ENTRY_PAGE_SIZE = "rvs.vote-entry-page-size";
+    private static final String PROPERTY_SORT_RESTAURANT = "rvs.sort-restaurant";
+    private static final String PROPERTY_SORT_RESTAURANT_WITH_MENU =
+            "rvs.sort-restaurant-with-menu";
+    private static final String PROPERTY_SORT_MENU_ENTRY = "rvs.sort-menu-entry";
+    private static final String PROPERTY_SORT_USER = "rvs.sort-user";
+    private static final String PROPERTY_SORT_VOTE_ENTRY = "rvs.sort-vote-entry";
+
     private static final int DEFAULT_RESTAURANT_PAGE_SIZE = 10;
     private static final int DEFAULT_MENU_ENTRY_PAGE_SIZE = 10;
     private static final int DEFAULT_USER_PAGE_SIZE = 10;
@@ -48,50 +59,75 @@ public class RvsPropertyResolver {
     private String sortVoteEntry;
 
     public LocalTime getMaxVoteTime() {
-        return Optional.ofNullable(maxVoteTime).orElse(DEFAULT_MAX_VOTE_TIME);
+        var optional = Optional.ofNullable(maxVoteTime);
+
+        if (optional.isPresent()) {
+            return optional.get();
+        } else {
+            log.warn("Property rvs.max-vote-time has incorrect value, using default value {}",
+                    DEFAULT_MAX_VOTE_TIME);
+
+            return DEFAULT_MAX_VOTE_TIME;
+        }
     }
 
     public int getRestaurantPageSize() {
-        return restaurantPageSize == 0 ? DEFAULT_RESTAURANT_PAGE_SIZE : restaurantPageSize;
+        return getDefaultPageSize(restaurantPageSize, DEFAULT_RESTAURANT_PAGE_SIZE,
+                PROPERTY_RESTAURANT_PAGE_SIZE);
     }
 
     public int getMenuEntryPageSize() {
-        return menuEntryPageSize == 0 ? DEFAULT_MENU_ENTRY_PAGE_SIZE : menuEntryPageSize;
+        return getDefaultPageSize(menuEntryPageSize, DEFAULT_MENU_ENTRY_PAGE_SIZE,
+                PROPERTY_MENU_ENTRY_PAGE_SIZE);
     }
 
     public int getUserPageSize() {
-        return userPageSize == 0 ? DEFAULT_USER_PAGE_SIZE : userPageSize;
+        return getDefaultPageSize(userPageSize, DEFAULT_USER_PAGE_SIZE, PROPERTY_USER_PAGE_SIZE);
     }
 
     public int getVoteEntryPageSize() {
-        return voteEntryPageSize == 0 ? DEFAULT_VOTE_ENTRY_PAGE_SIZE : voteEntryPageSize;
+        return getDefaultPageSize(voteEntryPageSize, DEFAULT_VOTE_ENTRY_PAGE_SIZE,
+                PROPERTY_VOTE_ENTRY_PAGE_SIZE);
     }
 
     public Sort getSortRestaurant() {
-        return getDefaultSort(sortRestaurant, RESTAURANT_PARAMS);
+        return getDefaultSort(sortRestaurant, PROPERTY_SORT_RESTAURANT, RESTAURANT_PARAMS);
     }
 
     public Sort getSortRestaurantWithMenu() {
-        return getDefaultSort(sortRestaurantWithMenu, RESTAURANT_WITH_MENU_PARAMS);
+        return getDefaultSort(sortRestaurantWithMenu, PROPERTY_SORT_RESTAURANT_WITH_MENU,
+                RESTAURANT_WITH_MENU_PARAMS);
     }
 
     public Sort getSortMenuEntry() {
-        return getDefaultSort(sortMenuEntry, MENU_ENTRY_PARAMS);
+        return getDefaultSort(sortMenuEntry, PROPERTY_SORT_MENU_ENTRY, MENU_ENTRY_PARAMS);
     }
 
     public Sort getSortUser() {
-        return getDefaultSort(sortUser, USER_PARAMS);
+        return getDefaultSort(sortUser, PROPERTY_SORT_USER, USER_PARAMS);
     }
 
     public Sort getSortVoteEntry() {
-        return getDefaultSort(sortVoteEntry, VOTE_ENTRY_PARAMS);
+        return getDefaultSort(sortVoteEntry, PROPERTY_SORT_VOTE_ENTRY, VOTE_ENTRY_PARAMS);
     }
 
-    private Sort getDefaultSort(String sortProperty, String... allowedSortParams) {
+    private int getDefaultPageSize(int value, int defaultValue, String propertyName) {
+        if (value > 0) {
+            return value;
+        } else {
+            log.warn("Property {} has incorrect value, using default value {}", propertyName,
+                    defaultValue);
+
+            return defaultValue;
+        }
+    }
+
+    private Sort getDefaultSort(String sortValue, String propertyName,
+                                String... allowedSortParams) {
         try {
-            return getSort(sortProperty, allowedSortParams).orElse(Sort.unsorted());
+            return getSort(sortValue, allowedSortParams).orElse(Sort.unsorted());
         } catch (IllegalArgumentException exception) {
-            log.warn("{} is incorrect sort property", sortRestaurant, exception);
+            log.warn("Property {} has incorrect value, sorting disabled", propertyName, exception);
 
             return Sort.unsorted();
         }
